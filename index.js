9832a9b19b8de44b31f747441474bdbffca0c632
阳九 <871921282@qq.com>
2023-03-19 23:25:00 +0800

    基本结构实现,实现syncHook,AsyncSeriesHook

---
+++ index.js
@@ -0,0 +1,11 @@
+const SyncHook = require('./lib/SyncHook')
+const SyncWaterfallHook = require('./lib/SyncWaterfallHook')
+const AsyncSeriesHook = require('./lib/AsyncSeriesHook')
+
+
+module.exports = {
+    SyncHook,
+    SyncWaterfallHook,
+    AsyncSeriesHook
+}
+
